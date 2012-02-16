5e94576906683ea9358bbc8f07c02bc4184f8107
Randy Unger <randy.unger@gmail.com>
2012-02-16 11:21:19 -0800

    Fixed typo in error message

--- util-eval/src/main/scala/com/twitter/util/Eval.scala
+++ util-eval/src/main/scala/com/twitter/util/Eval.scala
@@ -77,7 +77,7 @@ class Eval(target: Option[File]) {
     jarPathOfClass("scala.tools.nsc.Interpreter")
   } catch {
     case e =>
-      throw new RuntimeException("Unable lo load scala interpreter from classpath (scala-compiler jar is missing?)", e)
+      throw new RuntimeException("Unable to load scala interpreter from classpath (scala-compiler jar is missing?)", e)
   }
 
   private lazy val libPath = try {
